f0858e70c368843b62772a4bc747d56cd3dfa2bd
unknown <matthieu.raynaud-de-fitte@epitech.eu>
2020-07-24 17:15:25 +0200

    imported project from gitlab

---
+++ Assets/scripts/Selectable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selectable : MonoBehaviour {
+    public Renderer[] renderers; //Assign all child Mesh Renderers
+
+    public Bounds GetObjectBounds() {
+        Bounds totalBounds = new Bounds();
+
+        for (int i = 0; i < renderers.Length; i++) {
+            if (totalBounds.center == Vector3.zero) {
+                totalBounds = renderers[i].bounds;
+            }
+            else {
+                totalBounds.Encapsulate(renderers[i].bounds);
+            }
+        }
+        return totalBounds;
+    }
+
+    void OnEnable() {
+        //Add this Object to global list
+        if (!SelectionManager.selectables.Contains(this)) {
+            SelectionManager.selectables.Add(this);
+        }
+    }
+
+    void OnDisable() {
+        //Remove this Object from global list
+        if (SelectionManager.selectables.Contains(this)) {
+            SelectionManager.selectables.Remove(this);
+        }
+    }
+}
