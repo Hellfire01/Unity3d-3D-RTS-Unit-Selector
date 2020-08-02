763b6a3cb843ccc5da21f116fe920ff18eb4c7c9
unknown <matthieu.raynaud-de-fitte@epitech.eu>
2020-08-02 02:12:23 +0200

    get points of clipping planes from camera and distance

---
+++ Assets/scripts/GetClipPlanePoints.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetClipPlanePoints : MonoBehaviour {
+    public Camera mainCamera;
+    
+    public struct ClipPlanePoints {
+        public Vector3 UpperLeft;
+        public Vector3 UpperRight;
+        public Vector3 LowerLeft;
+        public Vector3 LowerRight;
+    }
+
+    private void Update() {
+        ClipPlanePoints tmp = getClipPlanePoints(mainCamera.nearClipPlane);
+        Debug.Log(tmp.LowerLeft + ", " + tmp.LowerRight);
+    }
+
+    public ClipPlanePoints getClipPlanePoints(float distance) {
+        ClipPlanePoints clipPlanePoints = new ClipPlanePoints();
+        Transform cameraTransform = mainCamera.transform;
+        Vector3 pos = cameraTransform.position;
+        float halfFOV = (mainCamera.fieldOfView * 0.5f) * Mathf.Deg2Rad;
+        float aspect = mainCamera.aspect;
+        float height = Mathf.Tan(halfFOV) * distance;
+        float width = height * aspect;
+
+        // lower right
+        clipPlanePoints.LowerRight = pos + cameraTransform.forward * distance;
+        clipPlanePoints.LowerRight += cameraTransform.right * width;
+        clipPlanePoints.LowerRight -= cameraTransform.up * height;
+        // lower left
+        clipPlanePoints.LowerLeft = pos + cameraTransform.forward * distance;
+        clipPlanePoints.LowerLeft -= cameraTransform.right * width;
+        clipPlanePoints.LowerLeft -= cameraTransform.up * height;
+        // upper right
+        clipPlanePoints.UpperRight = pos + cameraTransform.forward * distance;
+        clipPlanePoints.UpperRight += cameraTransform.right * width;
+        clipPlanePoints.UpperRight += cameraTransform.up * height;
+        // upper left
+        clipPlanePoints.UpperLeft = pos + cameraTransform.forward * distance;
+        clipPlanePoints.UpperLeft -= cameraTransform.right * width;
+        clipPlanePoints.UpperLeft += cameraTransform.up * height;
+        
+        return clipPlanePoints;
+    }
+}
