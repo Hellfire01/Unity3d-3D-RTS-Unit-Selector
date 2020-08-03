d6c7219efe97cbd7b293abcec8c28f18c428af98
Matthieu Raynaud de Fitte <matthieu.raynaud-de-fitte@outlook.com>
2020-08-03 15:08:59 +0200

    code cleaning

--- Assets/scripts/SelectionManager.cs
+++ Assets/scripts/SelectionManager.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(DrawSelectionIndicator))]
@@ -10,14 +8,13 @@ public class SelectionManager : MonoBehaviour {
     private Vector3 _mousePosition1;
     private List<int> _selectedObjectsIndex;
     private MeshFilter _selectionBoxMeshFilter;
+    private Bounds _cameraBounds;
     
     public static List<Selectable> selectables = new List<Selectable>();
     public Color selectionColor;
     public GameObject selectionMesh;
     public Camera mainCamera;
     
-    private Bounds _cameraBounds;
-
     private void Start() {
         _selectionStarted = false;
         _selectedObjectsIndex = new List<int>();
@@ -39,18 +36,22 @@ public class SelectionManager : MonoBehaviour {
         }
         // Detect which Objects are inside selection rectangle
         if (_selectionStarted) {
-            Camera camera = Camera.main;
             _selectedObjectsIndex.Clear();
             for (int i = 0; i < selectables.Count; i++) {
-                _cameraBounds = GetViewportBounds(camera, _mousePosition1, Input.mousePosition);
-                Debug.Log(_cameraBounds.ToString() + " / " + selectables[i].GetObjectBounds().ToString());
-                if (_cameraBounds.Contains(camera.WorldToViewportPoint(selectables[i].transform.position))) {
+                _cameraBounds = GetViewportBounds(_mousePosition1, Input.mousePosition);
+                // Debug.Log(_cameraBounds.ToString() + " / " + selectables[i].GetObjectBounds().ToString());
+                if (_cameraBounds.Contains(mainCamera.WorldToViewportPoint(selectables[i].transform.position))) {
                     _selectedObjectsIndex.Add(i);
                 }
             }
         }
+        Debug.Log(Input.mousePosition.ToString());
     }
 
+    private void UpdateSelectionMesh() {
+        Vector2 p0Ratio = new Vector2();
+    }
+    
     private void OnDrawGizmos() {
         // for (int i = 0; i < selectables.Count; i++) {
         //     Gizmos.color = Color.red;
@@ -71,21 +72,20 @@ public class SelectionManager : MonoBehaviour {
         }
         // Draw selection edges
         if (_selectedObjectsIndex.Count > 0) {
-            Camera camera = Camera.main;
             for (int i = 0; i < _selectedObjectsIndex.Count; i++) {
-                _dsi.DrawIndicator(camera, selectables[_selectedObjectsIndex[i]].GetObjectBounds());
+                _dsi.DrawIndicator(mainCamera, selectables[_selectedObjectsIndex[i]].GetObjectBounds());
             }
         }
     }
 
     // Defines a volume such as seen by the camera and delimited by the player
-    public Bounds GetViewportBounds(Camera camera, Vector3 screenPosition1, Vector3 screenPosition2) {
-        Vector3 v1 = camera.ScreenToViewportPoint(screenPosition1);
-        Vector3 v2 = camera.ScreenToViewportPoint(screenPosition2);
+    public Bounds GetViewportBounds(Vector3 screenPosition1, Vector3 screenPosition2) {
+        Vector3 v1 = mainCamera.ScreenToViewportPoint(screenPosition1);
+        Vector3 v2 = mainCamera.ScreenToViewportPoint(screenPosition2);
         Vector3 min = Vector3.Min(v1, v2);
         Vector3 max = Vector3.Max(v1, v2);
-        min.z = camera.nearClipPlane;
-        max.z = camera.farClipPlane;
+        min.z = mainCamera.nearClipPlane;
+        max.z = mainCamera.farClipPlane;
         Bounds bounds = new Bounds();
         bounds.SetMinMax(min, max);
         return bounds;
