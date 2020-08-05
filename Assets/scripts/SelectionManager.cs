8e4164954e8eb2e7403e0e4c71f53427d0d15e8a
Matthieu Raynaud de Fitte <matthieu.raynaud-de-fitte@outlook.com>
2020-08-05 16:39:31 +0200

    factorisation of the code in order to make debug easier

--- Assets/scripts/SelectionManager.cs
+++ Assets/scripts/SelectionManager.cs
@@ -25,7 +25,6 @@ public class SelectionManager : MonoBehaviour {
         CreatePrimitiveMesh.GenerateBoxMesh(_selectionBoxMeshFilter);
         _pointer = new GameObject();
         _pointer.name = "pointer for unit selection";
-        _pointer.transform.parent = transform.parent;
     }
     
     // Update is called once per frame
@@ -54,87 +53,26 @@ public class SelectionManager : MonoBehaviour {
         }
     }
 
-    // gets the ratios of the selection's rectangle ( ratios go from 0 to 1 )
-    // the ratios are then projected on the clipping planes in order to get the selection Mesh
     private void UpdateSelectionMeshValues() {
-        // == get the ratios of the 4 points of the selection rectangle ==
-        // top right
-        Vector2 p0Ratio = new Vector2(_selectionRect.xMax / Screen.width,
-                                      _selectionRect.yMin / Screen.height);
-        // top left
-        Vector2 p1Ratio = new Vector2(_selectionRect.xMin / Screen.width,
-                                      _selectionRect.yMin / Screen.height);
-        // bottom left
-        Vector2 p2Ratio = new Vector2(_selectionRect.xMin / Screen.width,
-                                      _selectionRect.yMax / Screen.height);
-        // bottom right
-        Vector2 p3Ratio = new Vector2(_selectionRect.xMax / Screen.width,
-                                      _selectionRect.yMax / Screen.height);
-
-        // == apply selection rectangle on near clip plane ==
-        GetClipPlanePoints.ClipPlanePoints nearClipPlanePoints = GetClipPlanePoints.getClipPlanePoints(mainCamera, mainCamera.nearClipPlane);
-        float nearPlaneWidth = Vector3.Distance(nearClipPlanePoints.LowerLeft, nearClipPlanePoints.LowerRight);
-        float nearPlaneHeight = Vector3.Distance(nearClipPlanePoints.UpperRight, nearClipPlanePoints.LowerRight);
-        _pointer.transform.eulerAngles = mainCamera.transform.eulerAngles;
-        // p0
-        _pointer.transform.position = nearClipPlanePoints.LowerLeft;
-        _pointer.transform.Translate(nearPlaneWidth * p0Ratio.x, nearPlaneHeight * p0Ratio.y, 0);
-        Vector3 p0 = _pointer.transform.position;
-        // p1
-        _pointer.transform.position = nearClipPlanePoints.LowerLeft;
-        _pointer.transform.Translate(nearPlaneWidth * p1Ratio.x, nearPlaneHeight * p1Ratio.y, 0);
-        Vector3 p1 = _pointer.transform.position;
-        // p2
-        _pointer.transform.position = nearClipPlanePoints.LowerLeft;
-        _pointer.transform.Translate(nearPlaneWidth * p2Ratio.x, nearPlaneHeight * p2Ratio.y, 0);
-        Vector3 p2 = _pointer.transform.position;
-        // p3
-        _pointer.transform.position = nearClipPlanePoints.LowerLeft;
-        _pointer.transform.Translate(nearPlaneWidth * p3Ratio.x, nearPlaneHeight * p3Ratio.y, 0);
-        Vector3 p3 = _pointer.transform.position;
-        // == apply selection rectangle on far clip plane ==
-        GetClipPlanePoints.ClipPlanePoints farClipPlanePoints = GetClipPlanePoints.getClipPlanePoints(mainCamera, mainCamera.farClipPlane);
-        float farPlaneWidth = Vector3.Distance(farClipPlanePoints.LowerLeft, farClipPlanePoints.LowerRight);
-        float farPlaneHeight = Vector3.Distance(farClipPlanePoints.UpperRight, farClipPlanePoints.LowerRight);
-        _pointer.transform.eulerAngles = mainCamera.transform.eulerAngles;
-        // p4
-        _pointer.transform.position = farClipPlanePoints.LowerLeft;
-        _pointer.transform.Translate(farPlaneWidth * p0Ratio.x, farPlaneHeight * p0Ratio.y, 0);
-        Vector3 p4 = _pointer.transform.position;
-        // p5
-        _pointer.transform.position = farClipPlanePoints.LowerLeft;
-        _pointer.transform.Translate(farPlaneWidth * p1Ratio.x, farPlaneHeight * p1Ratio.y, 0);
-        Vector3 p5 = _pointer.transform.position;
-        // p6
-        _pointer.transform.position = farClipPlanePoints.LowerLeft;
-        _pointer.transform.Translate(farPlaneWidth * p2Ratio.x, farPlaneHeight * p2Ratio.y, 0);
-        Vector3 p6 = _pointer.transform.position;
-        // p7
-        _pointer.transform.position = farClipPlanePoints.LowerLeft;
-        _pointer.transform.Translate(farPlaneWidth * p3Ratio.x, farPlaneHeight * p3Ratio.y, 0);
-        Vector3 p7 = _pointer.transform.position;
+        SelectionMeshVerticesCalc smvc = new SelectionMeshVerticesCalc(mainCamera, _selectionRect);
         // == vertices of the mesh ==
         Vector3[] vertices = new Vector3[] {
             // Bottom
-            p0, p1, p2, p3,
+            smvc.p0, smvc.p1, smvc.p2, smvc.p3,
             // Left
-            p7, p4, p0, p3,
+            smvc.p7, smvc.p4, smvc.p0, smvc.p3,
             // Front
-            p4, p5, p1, p0,
+            smvc.p4, smvc.p5, smvc.p1, smvc.p0,
             // Back
-            p6, p7, p3, p2,
+            smvc.p6, smvc.p7, smvc.p3, smvc.p2,
             // Right
-            p5, p6, p2, p1,
+            smvc.p5, smvc.p6, smvc.p2, smvc.p1,
             // Top
-            p7, p6, p5, p4
+            smvc.p7, smvc.p6, smvc.p5, smvc.p4
         };
         _selectionBoxMeshFilter.mesh.vertices = vertices;
     }
-    
-    private void UpdateSelectionMesh() {
-        Vector2 p0Ratio = new Vector2();
-    }
-    
+
     private void OnDrawGizmos() {
         // for (int i = 0; i < selectables.Count; i++) {
         //     Gizmos.color = Color.red;
---
+++ Assets/scripts/SelectionMesh/SelectionMeshVerticesCalc.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionMeshVerticesCalc {
+    public Vector2 p0Ratio;
+    public Vector2 p1Ratio;
+    public Vector2 p2Ratio;
+    public Vector2 p3Ratio;
+
+    public Vector3 p0;
+    public Vector3 p1;
+    public Vector3 p2;
+    public Vector3 p3;
+    public Vector3 p4;
+    public Vector3 p5;
+    public Vector3 p6;
+    public Vector3 p7;
+
+    private Camera _mainCamera;
+    private Rect _selectionRect;
+    private GameObject _pointer;
+    
+    public SelectionMeshVerticesCalc(Camera mainCamera, Rect selectionRect) {
+        _mainCamera = mainCamera;
+        _selectionRect = selectionRect;
+        _pointer = new GameObject();
+        _pointer.name = "pointer for pos of vertices of select mesh";
+        getRatios();
+        getVertices(out p0, out p1, out p2, out p3, _mainCamera.nearClipPlane);
+        getVertices(out p4, out p5, out p6, out p7, _mainCamera.farClipPlane);
+    }
+
+    // gets the ratios of the selection's rectangle ( ratios go from 0 to 1 )
+    private void getRatios() {
+        // top right
+        p0Ratio = new Vector2(_selectionRect.xMax / Screen.width,
+                              _selectionRect.yMin / Screen.height);
+        // top left
+        p1Ratio = new Vector2(_selectionRect.xMin / Screen.width,
+                              _selectionRect.yMin / Screen.height);
+        // bottom left
+        p2Ratio = new Vector2(_selectionRect.xMin / Screen.width,
+                              _selectionRect.yMax / Screen.height);
+        // bottom right
+        p3Ratio = new Vector2(_selectionRect.xMax / Screen.width,
+                              _selectionRect.yMax / Screen.height);
+    }
+
+    // apply selection rectangle on given clip plane
+    private void getVertices(out Vector3 pA, out Vector3 pB, out Vector3 pC, out Vector3 pD, float distance) {
+        GetClipPlanePoints.ClipPlanePoints nearClipPlanePoints = GetClipPlanePoints.getClipPlanePoints(_mainCamera, distance);
+        float nearPlaneWidth = Vector3.Distance(nearClipPlanePoints.LowerLeft, nearClipPlanePoints.LowerRight);
+        float nearPlaneHeight = Vector3.Distance(nearClipPlanePoints.UpperRight, nearClipPlanePoints.LowerRight);
+        _pointer.transform.eulerAngles = _mainCamera.transform.eulerAngles;
+        // pA
+        _pointer.transform.position = nearClipPlanePoints.LowerLeft;
+        _pointer.transform.Translate(nearPlaneWidth * p0Ratio.x, nearPlaneHeight * p0Ratio.y, 0);
+        pA = _pointer.transform.position;
+        // pB
+        _pointer.transform.position = nearClipPlanePoints.LowerLeft;
+        _pointer.transform.Translate(nearPlaneWidth * p1Ratio.x, nearPlaneHeight * p1Ratio.y, 0);
+        pB = _pointer.transform.position;
+        // pC
+        _pointer.transform.position = nearClipPlanePoints.LowerLeft;
+        _pointer.transform.Translate(nearPlaneWidth * p2Ratio.x, nearPlaneHeight * p2Ratio.y, 0);
+        pC = _pointer.transform.position;
+        // pD
+        _pointer.transform.position = nearClipPlanePoints.LowerLeft;
+        _pointer.transform.Translate(nearPlaneWidth * p3Ratio.x, nearPlaneHeight * p3Ratio.y, 0);
+        pD = _pointer.transform.position;
+    }
+}
